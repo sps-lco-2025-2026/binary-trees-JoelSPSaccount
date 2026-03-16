35d73029761b11d7cb20c66e3c2b389449521c77
JoelSPSaccount <patterj@stpaulsschool.org.uk>
2026-03-16 12:27:02 +0000

    Update BasicBinaryTreeTests.cs

--- BinaryTreeIntroduction/BinaryTreeIntroduction.Tests/BasicBinaryTreeTests.cs
+++ BinaryTreeIntroduction/BinaryTreeIntroduction.Tests/BasicBinaryTreeTests.cs
@@ -16,64 +16,65 @@ public sealed class BasicLinkedListTests
     [TestMethod]
     public void TestCount()
     {
-        var ill = new BinaryTree(5);
-        ill.Insert(7);
-        ill.Insert(9);
-        ill.Insert(9);
-        Assert.AreEqual(4, ill.Count);
+        var bt = new BinaryTree(5);
+        bt.Insert(7);
+        bt.Insert(9);
+        bt.Insert(9);
+        Assert.AreEqual(4, bt.Count);
     }
 
     [TestMethod]
     public void TestSum()
     {
-        var ill = new BinaryTree(5);
-        ill.Insert(7);
-        ill.Insert(9);
-        Assert.AreEqual(21, ill.Sum);
+        var bt = new BinaryTree(5);
+        bt.Insert(7);
+        bt.Insert(9);
+        Assert.AreEqual(21, bt.Sum);
     }
 
     [TestMethod]
     public void TestToStringExplicit()
     {
-        var ill = new BinaryTree(5);
-        ill.Insert(7);
-        ill.Insert(9);
-        Assert.AreEqual("{5, 7, 9}", ill.ToString());
+        var bt = new BinaryTree(5);
+        bt.Insert(7);
+        bt.Insert(9);
+        Assert.AreEqual("{5, 7, 9}", bt.ToString());
     }
     [TestMethod]
     public void TestSearch()
     {
-        var ill = new BinaryTree(7);
-        ill.Insert(4);
-        ill.Insert(9);
-        ill.Insert(3);
-        ill.Insert(5);
-        Assert.IsTrue(ill.Contains(5));
+        var bt = new BinaryTree(7);
+        bt.Insert(4);
+        bt.Insert(9);
+        bt.Insert(3);
+        bt.Insert(5);
+        Assert.IsTrue(bt.Contains(5));
     }
 
     [TestMethod]
     public void TestDeletion()
     {
-        var ill = new BinaryTree(7);
-        ill.Insert(4);
-        ill.Insert(9);
-        ill.Insert(5);
-        Assert.IsTrue(ill.Delete(5));
-        Assert.IsFalse(ill.Delete(5));
-        Assert.IsFalse(ill.Contains(5));
-        Assert.AreEqual(3, ill.Count);
+        var bt = new BinaryTree(7);
+        bt.Insert(4);
+        bt.Insert(9);
+        bt.Insert(5);
+        Assert.IsTrue(bt.Delete(5));
+        Assert.IsFalse(bt.Delete(5));
+        Assert.IsFalse(bt.Contains(5));
+        Assert.AreEqual(3, bt.Count);
     }
 
     [TestMethod]
     public void TestBalance()
     {
-        var ill = new BinaryTree(4);
-        ill.Insert(2);
-        ill.Insert(6);
-        ill.Insert(1);
-        ill.Insert(3);//    4
-        ill.Insert(5);//  2---6
-        ill.Insert(7);//1--3 5--7
-        Assert.IsTrue(ill.IsBalanced);
+        var bt = new BinaryTree(4);
+        bt.Insert(2);
+        bt.Insert(6);
+        bt.Insert(1);
+        bt.Insert(3);//    4
+        bt.Insert(5);//  2---6
+        bt.Insert(7);//1--3 5--7
+        Assert.IsTrue(bt.IsBalanced);
+        Assert.IsTrue(false);
     }
 }
